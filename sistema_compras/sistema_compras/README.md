c5f70029560ce14315b5178896f06dd82f3d6214
steven123 <steven40020@gmail.com>
2025-10-13 00:05:29 -0500

    se configura swagger y se agrega README para dar mas informacion sobre el proyecto

---
+++ sistema_compras/sistema_compras/README.md
@@ -0,0 +1,31 @@
+# Sistema de Compras — API REST
+
+**Proyecto desarrollado con Spring Boot 3, JPA, Lombok, MapStruct y Swagger UI.**  
+Permite la gestión de categorías, productos, proveedores, compras y usuarios dentro de un sistema de compras modular y escalable.
+
+---
+
+##  Tecnologías usadas
+
+- **Java 17**
+- **Spring Boot 3.5.5**
+- **Spring Data JPA (Hibernate)**
+- **Spring Security 6 (BCryptPasswordEncoder)**
+- **Spring Validation**
+- **MapStruct 1.5.5.Final**
+- **Lombok**
+- **MySQL**
+- **Swagger (Springdoc OpenAPI 2.5.0)**
+
+---
+
+##  Configuración del proyecto
+
+### Clona el repositorio
+```bash
+git clone https://github.com/tu-usuario/sistema_compras.git
+cd sistema_compras
+
+## URL para probar ENDPOINTS
+
+http://localhost:8080/swagger-ui/index.html
--- sistema_compras/sistema_compras/src/main/java/co/los_inges/sistema_compras/utils/SecurityConfig.java
+++ sistema_compras/sistema_compras/src/main/java/co/los_inges/sistema_compras/utils/SecurityConfig.java
@@ -4,13 +4,37 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(basic -> {})
+                .formLogin(form -> form
+                        .permitAll()
+                );
+
+        return http.build();
     }
 }
